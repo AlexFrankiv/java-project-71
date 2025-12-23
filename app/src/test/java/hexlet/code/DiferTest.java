0c903e2de29f5389df700912a054b15b9aea8b3a
franAlex <frankivs@mail.ru>
2025-12-23 18:45:45 +0500

    fix checkstyle problem 2.

--- app/src/test/java/hexlet/code/DiferTest.java
+++ app/src/test/java/hexlet/code/DiferTest.java
@@ -2,10 +2,13 @@ package hexlet.code;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.nio.file.Files;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class DiferTest {
     @Test
@@ -24,5 +27,17 @@ public class DiferTest {
         assertTrue(result.contains("- timeout: 50"));
         assertTrue(result.contains("+ timeout: 20"));
         assertTrue(result.contains("+ verbose: true"));
+    }
+    @Test
+    void testAllMethods() throws Exception {
+        File temp1 = Files.createTempFile("test1", ".json").toFile();
+        File temp2 = Files.createTempFile("test2", ".json").toFile();
+        Files.writeString(temp1.toPath(), "{}");
+        Files.writeString(temp2.toPath(), "{}");
+
+        Differ.generate(temp1, temp2, "stylish");
+
+        assertThrows(Exception.class, () ->
+                Differ.generate(new File("fake1.json"), new File("fake2.json"), "stylish"));
     }
 }
